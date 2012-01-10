abfc81f7ff396e71fbe1805d56ead3f4cb5681be
Mariano Luna <marianol@moondaysolutions.com>
2012-01-10 15:20:41 -0600

    Array to Combo box function

--- resources/functions.php
+++ resources/functions.php
@@ -13,4 +13,40 @@ function decoratePageTabs($tabArray, $selectedTab = -1) {
 	}
 	return $output;
 }
+
+/**
+ * Creates a combo box from an array 
+ *
+ * @param string $ComboName
+ * @param string $SelectedValue
+ * @param array $array
+ * @param string $css
+ * @param string $atributo
+ * @param boolean $selectall
+ * @return string
+ */
+function makeSelectArray($ComboName, $SelectedValue, $array, $css = "", $atributo = "", $selectall = false)
+{
+	
+	$combo_box = "";
+	$combo_box .= '<select name="'.$ComboName.'" class="'.$css.'" '.$atributo.' >'."\n";
+	
+	if ($selectall) {   
+		$combo_box .= '<option value="_ALL_">ALL</option>'."\n";
+	} else {
+		$combo_box .= '<option value=""></option>'."\n";
+	}
+
+	foreach ($array as $id => $optionvalue)	{
+		if ($id === $SelectedValue) {
+			$selection =  ' SELECTED ';
+		} else {
+			$selection = ($SelectedValue != null && $SelectedValue != '' && $id == $SelectedValue) ? ' SELECTED ' : '';
+		}
+		$combo_box .= '<option value="'. $id .'" '. $selection .'>'. $optionvalue .'</option>'."\n";
+	}
+	$combo_box .= "</select>\n";
+
+	return $combo_box;
+}
 ?>
