61734a9ea9156ffa63a07f1a2628a57590687d0b
Mariano Luna <marianol@moondaysolutions.com>
2012-07-24 12:38:50 -0500

    simple iframe test

--- web_root/iframe.php
+++ web_root/iframe.php
@@ -93,7 +93,7 @@ switch ($myPath) {
 }
 
 
-$myIframeSRC = ($myIframeSRC == '') ? '"' . $iFrameServerURI . $iFramePath . $iFrameLoginInfo . '"' : '"' . $myIframeSRC . '"';
+$myIframeSRC = ($myIframeSRC == '') ? '"' . $iFrameServerURI . $iFramePath . $iFrameLoginInfo . '"' : '"' . $myIframeSRC; // . '&j_username=jasperadmin&j_password=jasperadmin"';
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
