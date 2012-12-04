0a68e5c1e65ba3f4824f567279f15f5edeb8a3c1
Mariano Luna <marianolu@gmail.com>
2012-12-04 14:29:08 -0600

    Bug to make the sample work with Jasper Server 5.0

--- resources/functions.php
+++ resources/functions.php
@@ -1,17 +1,17 @@
 <?php
 
 function decorateError($errorText, $errorType = '') {
-	return '<div class="error">' . $errorText . '</div>';
+    return '<div class="error">' . $errorText . '</div>';
 }
 
 function decoratePageTabs($tabArray, $selectedTab = -1) {
-	$output = '';
-	foreach ($tabArray as $item => $legend) {
-		$selected = ($selectedTab == $item) ? 'selected' : '';
-	    $output .= '<li class="' . $selected . '">' . $legend . '</li>' . "\n";
+    $output = '';
+    foreach ($tabArray as $item => $legend) {
+        $selected = ($selectedTab == $item) ? 'selected' : '';
+        $output .= '<li class="' . $selected . '">' . $legend . '</li>' . "\n";
 
-	}
-	return $output;
+    }
+    return $output;
 }
 
 /**
@@ -27,27 +27,43 @@ function decoratePageTabs($tabArray, $selectedTab = -1) {
  */
 function makeSelectArray($ComboName, $SelectedValue, $array, $css = "", $atributo = "", $emptyitem = false, $selectall = false)
 {
-	
-	$combo_box = "";
-	$combo_box .= '<select name="'.$ComboName.'" class="'.$css.'" '.$atributo.' >'."\n";
-	
-	if ($selectall) {   
-		$combo_box .= '<option value="_ALL_">ALL</option>'."\n";
-	} 
-	if ($emptyitem) {
-		$combo_box .= '<option value=""></option>'."\n";
-	}
+    
+    $combo_box = "";
+    $combo_box .= '<select name="'.$ComboName.'" class="'.$css.'" '.$atributo.' >'."\n";
+    
+    if ($selectall) {   
+        $combo_box .= '<option value="_ALL_">ALL</option>'."\n";
+    } 
+    if ($emptyitem) {
+        $combo_box .= '<option value=""></option>'."\n";
+    }
 
-	foreach ($array as $id => $optionvalue)	{
-		if ($id === $SelectedValue) {
-			$selection =  ' SELECTED ';
-		} else {
-			$selection = ($SelectedValue != null && $SelectedValue != '' && $id == $SelectedValue) ? ' SELECTED ' : '';
-		}
-		$combo_box .= '<option value="'. $id .'" '. $selection .'>'. $optionvalue .'</option>'."\n";
-	}
-	$combo_box .= "</select>\n";
+    foreach ($array as $id => $optionvalue) {
+        if ($id === $SelectedValue) {
+            $selection =  ' SELECTED ';
+        } else {
+            $selection = ($SelectedValue != null && $SelectedValue != '' && $id == $SelectedValue) ? ' SELECTED ' : '';
+        }
+        $combo_box .= '<option value="'. $id .'" '. $selection .'>'. $optionvalue .'</option>'."\n";
+    }
+    $combo_box .= "</select>\n";
 
-	return $combo_box;
+    return $combo_box;
 }
+
+function fix_session_register() {
+    function session_register() {
+        $args = func_get_args();
+        foreach ($args as $key) {
+            $_SESSION[$key]=$GLOBALS[$key];
+        }
+    }
+    function session_is_registered($key) {
+        return isset($_SESSION[$key]);
+    }
+    function session_unregister($key) {
+        unset($_SESSION[$key]);
+    }
+}
+if (!function_exists('session_register')) fix_session_register();
 ?>
