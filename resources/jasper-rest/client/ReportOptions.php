c3d737c45140ef84ff15c621887a0ab86ef011b2
Mariano Luna <marianolu@gmail.com>
2012-12-28 13:34:31 -0600

    added new Input Structure class to support Listing Input Control Structure form Rest V2 Jasper Web Services Guide Section 3.3.1

--- resources/jasper-rest/client/ReportOptions.php
+++ resources/jasper-rest/client/ReportOptions.php
@@ -78,6 +78,19 @@ class InputOptions {
 		return $result;
 	}
 
+    public static function createFromArray($data_array) {
+        $result = array();
+        foreach($data_array['inputControlState'] as $k) {
+            $temp = new self($k['uri'], $k['id'], $k['value'], $k['error']);
+            if (!empty($k['options'])) {
+                foreach ($k['options'] as $o) {
+                    $temp->addOption($o['label'], $o['value'], $o['selected']);
+                }
+            }
+            $result[] = $temp;
+        }
+        return $result;
+    }
 	public function addOption($label, $value, $selected) {
 		$temp = array('label' => strval($label), 'value' => strval($value), 'selected' => $selected);
 		if($selected == 1) { $temp['selected'] = 'true'; } else { $temp['selected'] = 'false'; }
@@ -110,56 +123,57 @@ class InputOptions {
 
 class InputStructure {
 
-    public $uri;
     public $id;
-    public $value;
-    public $error;
-    public $options = array();
+    public $type;
+    public $uri;
+    public $label;
+    public $mandatory;
+    public $readOnly;
+    public $visible;
+    public $masterDependencies = array();
+    public $slaveDependencies = array();
+    public $validationRules = array();
+    public $inputOptions = array();
 
-    public function __construct($uri = null, $id = null, $value = null, $error = null) {
+    public function __construct($uri = null, $id = null, $type = null, $label = null, 
+                                $mandatory = null, $readOnly = null, $visible = null) {
         $this->uri = (!empty($uri)) ? strval($uri) : null;
         $this->id = (!empty($id)) ? strval($id) : null;
-        $this->value = (!empty($value)) ? strval($value) : null;
-        $this->error = (!empty($error)) ? strval($error) : null;
+        $this->type = (!empty($type)) ? strval($type) : null;
+        $this->label = (!empty($label)) ? strval($label) : null;
+        $this->mandatory = (!empty($mandatory)) ? $mandatory : null;
+        $this->readOnly = (!empty($readOnly)) ? $readOnly : null;
+        $this->visible = (!empty($visible)) ? $visible : null;
     }
 
     public static function createFromJSON($json) {
         $data_array = json_decode($json, true);
         $result = array();
-        foreach($data_array['inputControlState'] as $k) {
-            $temp = new self($k['uri'], $k['id'], $k['value'], $k['error']);
-            if (!empty($k['options'])) {
-                foreach ($k['options'] as $o) {
-                    $temp->addOption($o['label'], $o['value'], $o['selected']);
-                }
+        foreach($data_array['inputControl'] as $k) {
+            $temp = new self($k['uri'], $k['id'], $k['type'], $k['label'], $k['mandatory'], $k['readOnly'],
+                             $k['visible']);
+            if (!empty($k['state'])) {
+                    $temp->inputOptions = InputOptions::createFromArray($k['state']);
             }
+            $temp->masterDependencies = $k['masterDependecies'];
+            $temp->slaveDependencies = $k['slaveDependencies'];
+            $temp->validationRules = $k['validationRules'];
             $result[] = $temp;
         }
         return $result;
     }
 
-    public function addOption($label, $value, $selected) {
-        $temp = array('label' => strval($label), 'value' => strval($value), 'selected' => $selected);
-        if($selected == 1) { $temp['selected'] = 'true'; } else { $temp['selected'] = 'false'; }
-        $this->options[] = $temp;
-    }
 
     public function getOptions() {
-        return $this->options;
+        return $this->inputOptions;
     }
 
     public function getUri() {
         return $this->uri;
     }
 
-    public function getSelected() {
-        $selectedValues = array();
-        foreach ($this->options as $opt) {
-            if ($opt['selected'] == 'true') {
-                $selectedValues[] = $opt['value'];
-            }
-        }
-        return $selectedValues;
+    public function getType() {
+        return $this->type;
     }
     
     public function getId() {
